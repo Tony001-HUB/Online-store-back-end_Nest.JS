df63d41ac78767a677497ce267aed06b866f9b97
Anton Asipenka <aasipenka@elinext.com>
2022-01-15 20:04:35 +0300

    Added modal for news table

---
+++ back-end/src/news/models/news.model.ts
@@ -0,0 +1,25 @@
+import {  Column, DataType, Model, Table } from "sequelize-typescript";
+
+interface INewsCreationAttrs {
+    title: string;
+    country: string;
+    link: string;
+}
+
+@Table({tableName: 'news'})
+export class News extends Model<News, INewsCreationAttrs> {
+    
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    id: number;
+
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    title: string;
+
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    country: string;
+
+    @Column({type: DataType.STRING, unique: true, allowNull: false})
+    link: string;
+    
+}
+   
