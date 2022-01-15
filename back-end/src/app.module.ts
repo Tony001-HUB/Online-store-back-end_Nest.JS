dfee678d9bef661d1fe1fc4c425b994320fad239
Anton Asipenka <aasipenka@elinext.com>
2022-01-15 19:47:20 +0300

    Added structure for news

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewsController } from './news/news.controller';
+import { NewsService } from './news/news.service';
+import { NewsModule } from './news/news.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRESS_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true
+    }),
+    NewsModule,
+  ],
+  controllers: [AppController, NewsController],
+  providers: [AppService, NewsService],
 })
 export class AppModule {}
