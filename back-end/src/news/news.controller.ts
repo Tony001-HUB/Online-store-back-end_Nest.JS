2b1b2d485d48264b587326b9dbc73be89f5b996f
Anton Asipenka <aasipenka@elinext.com>
2022-01-15 20:25:17 +0300

    Added logic for NewsController

--- back-end/src/news/news.controller.ts
+++ back-end/src/news/news.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { NewsDto } from './dto/news.dto';
+import { NewsService } from './news.service';
 
 @Controller('news')
-export class NewsController {}
+export class NewsController {
+    constructor(private newsService: NewsService) {}
+
+    @Post()
+    public createNews(@Body() newsDto: NewsDto) {
+        return this.newsService.createNews(newsDto);
+    }
+
+    @Get()
+    public getAllNews() {
+      return this.newsService.getAllNews();
+    }
+
+    @Get('/:value')
+    public getNewsByTitle(@Param('title') title: string) {
+      return this.newsService.getNewsByTitle(title);
+    }
+}
