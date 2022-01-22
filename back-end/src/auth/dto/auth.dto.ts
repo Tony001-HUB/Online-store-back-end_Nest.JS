1aeb425c3477f2f85d1d15fea1b1fdeeca2e0ace
Anton Asipenka <aasipenka@elinext.com>
2022-01-22 13:02:51 +0300

    Connection to a remote database has been added

--- back-end/src/auth/dto/auth.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class AuthUserDto {
-  @ApiProperty({example: 'user@gmail.com', description: 'User e-mail'})
-  readonly email: string;
-  @ApiProperty({example: 'QWERTY12345', description: 'User password'})
-  readonly password: string;
-}
